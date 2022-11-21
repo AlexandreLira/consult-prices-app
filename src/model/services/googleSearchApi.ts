9e7812f5d2445558757cc515af0cdee290044684
AlexandreLira <alexandrelira112@gmail.com>
2022-11-21 20:11:22 -0300

    removing console.log

--- src/model/services/googleSearchApi.ts
+++ src/model/services/googleSearchApi.ts
@@ -23,7 +23,6 @@ export async function googleSearchApi(name: string){
         throw new Error(data.error)
     }
     const title = formattedTitle(data.items[0].title, name)
-    console.log('title: ', title)
     return title
 }
 
